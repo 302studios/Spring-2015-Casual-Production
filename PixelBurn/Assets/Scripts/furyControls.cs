f2e2952dd8fe6e74272fbe40b6ed19938231576f
302studios <302devs@gmail.com>
2015-03-04 18:10:42 -0500

    Needs Work

--- PixelBurn/Assets/Scripts/furyControls.cs
+++ PixelBurn/Assets/Scripts/furyControls.cs
@@ -12,6 +12,9 @@ public class furyControls : MonoBehaviour {
 	Vector3 defaultCamLoc;
 	float defaultCamSize;
 
+	public GameObject slideBar;
+	public Slider theSlide;
+
 	public GameObject playerOneChar;
 	public GameObject playerTwoChar;
 	Vector3 currLoc;
@@ -29,9 +32,13 @@ public class furyControls : MonoBehaviour {
 	int p1Choice;
 	int p2Choice;
 
+	public int player1track = 1;
+	public int player2track = 1;
+	
 	public Text topText;
 	public Text bottomText;
 
+	bool inStruggle;
 	bool inReveal;
 
 	// Use this for initialization
@@ -50,6 +57,12 @@ public class furyControls : MonoBehaviour {
 		p2Select = GameObject.Find ("Player 2 Select");
 
 		winner = 0;
+
+		inStruggle = false;
+
+		slideBar.gameObject.SetActive(false);
+
+	
 	}
 	
 	// Update is called once per frame
@@ -82,6 +95,8 @@ public class furyControls : MonoBehaviour {
 			spotLight.transform.localScale = new Vector3 (1.8f, 1.8f, 1.8f);
 			spotLight.GetComponent<SpriteRenderer> ().color = new Color (255, 255, 255, 255);
 
+			slideBar.gameObject.SetActive(false);
+
 			sR.transform.position = furyLoc;
 			sR.GetComponent<SpriteRenderer> ().color = new Color (255, 255, 255, 255);
 
@@ -105,29 +120,29 @@ public class furyControls : MonoBehaviour {
 
 
 		if(!p1HasChosen){
-			if(Input.GetKeyDown(KeyCode.Z)){
+			if(Input.GetKeyDown(KeyCode.Z) && !inStruggle){
 			   p1Choice = 1;
 				p1HasChosen = true;
 			}
-			if(Input.GetKeyDown(KeyCode.X)){
+			if(Input.GetKeyDown(KeyCode.X) && !inStruggle){
 				p1Choice = 2;
 				p1HasChosen = true;
 			}
-			if(Input.GetKeyDown(KeyCode.C)){
+			if(Input.GetKeyDown(KeyCode.C) && !inStruggle){
 				p1Choice = 3;
 				p1HasChosen = true;
 			}
 		}
 		if(!p2HasChosen){
-			if(Input.GetKeyDown(KeyCode.J)){
+			if(Input.GetKeyDown(KeyCode.J) && !inStruggle){
 				p2Choice = 1;
 				p2HasChosen = true;
 			}
-			if(Input.GetKeyDown(KeyCode.K)){
+			if(Input.GetKeyDown(KeyCode.K) && !inStruggle){
 				p2Choice = 2;
 				p2HasChosen = true;
 			}
-			if(Input.GetKeyDown(KeyCode.L)){
+			if(Input.GetKeyDown(KeyCode.L) && !inStruggle){
 				p2Choice = 3;
 				p2HasChosen = true;
 			}
@@ -147,15 +162,15 @@ public class furyControls : MonoBehaviour {
 
 		if ((p1 == 1) && (p2 == 1)) {
 			bottomText.text = "TIE!! Player 1 Default Wins";
-			return tugOfWar ();
+			return struggle ();
 		}
 		if ((p1 == 2) && (p2 == 2)) {
 						bottomText.text = "TIE!! Player 1 Default Wins";
-						return tugOfWar ();
+			return struggle ();
 				}
 		if ((p1 == 3) && (p2 == 3)) {
 						bottomText.text = "TIE!! Player 1 Default Wins";
-						return tugOfWar ();
+			return struggle ();
 				}
 
 		// Player 1 Victory
@@ -190,9 +205,64 @@ public class furyControls : MonoBehaviour {
 
 	}
 
-	int tugOfWar(){
+	bool countingDown = false;
 
-		return 1;
+	int struggle(){
+
+		slideBar.gameObject.SetActive(true);
+
+		int total = player1track + player2track;
+
+		inStruggle = true;
+
+		Debug.Log("Check 1");
+
+
+		countingDown = true;
+		countdown();
+
+		while(countingDown){
+			Debug.Log("Check 2");
+			// Player 1 Struggle Input
+			if((Input.GetKeyDown(KeyCode.Z)) || (Input.GetKeyDown(KeyCode.X)) || (Input.GetKeyDown(KeyCode.C))){
+
+				player1track++;
+
+			}
+
+			// Player 2 Struggle Input
+			if((Input.GetKeyDown(KeyCode.J)) || (Input.GetKeyDown(KeyCode.K)) || (Input.GetKeyDown(KeyCode.L))){
+				
+				player2track++;
+				
+			}
+			Debug.Log("Check 3");
+
+			total = player1track + player2track;
+			theSlide.value = total/player1track;
+			Debug.Log("Check 4");
+
+		}
+
+	
+		inStruggle = false;
+
+		if(player1track >= player2track)
+			return 1;
+		else
+			return 2;
+		//return 1;
+
+	}
+
+
+	// IEnumerator breaking the code!!! Infinite while loop! Coroutine in coroutine?
+	IEnumerator countdown(){
+
+		//countingDown = true;
+		yield return new WaitForSeconds(7);
+		countingDown = false;
+
 
 	}
 
@@ -203,6 +273,7 @@ public class furyControls : MonoBehaviour {
 			cam.GetComponent<Camera> ().orthographicSize = defaultCamSize;
 			sR.GetComponent<SpriteRenderer> ().color = new Color (0, 0, 0, 0);
 			spotLight.GetComponent<SpriteRenderer> ().color = new Color (0, 0, 0, 0);
+			slideBar.gameObject.SetActive(false);
 			topText.color = new Color (0, 0, 0, 0);
 			bottomText.color = new Color (0, 0, 0, 0);
 
